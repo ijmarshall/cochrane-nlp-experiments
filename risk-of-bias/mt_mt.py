85b7470fcb45858734f656bc7a8112530be7690c
Iain Marshall <mail@ijmarshall.com>
2014-08-11 17:15:59 +0100

    sanity check predicted sentences

--- risk-of-bias/mt_mt.py
+++ risk-of-bias/mt_mt.py
@@ -142,7 +142,7 @@ def main(out_dir="results"):
 
     vec = modhashvec.ModularVectorizer(norm=None, non_negative=True, binary=True, ngram_range=(1, 2), n_features=2**26) # since multitask + bigrams = huge feature space
     vec.builder_clear()
-    vec.builder_add_docs(docs.X(uids_train), low=10) # add base features
+    vec.builder_add_docs(docs.X(uids_train), low=7) # add base features
     vec.builder_add_docs(docs.Xyi(uids_train), low=2) # add domain interactions
     # removed X_train_d since already been through the generator! (needed reset)
     vec.builder_add_docs(izip(high_prob_sents, interaction_domains), low=2)    # then add sentence interaction terms
@@ -188,6 +188,11 @@ def main(out_dir="results"):
         
         sent_domain_interactions = ["-s-" + domain] * len(high_prob_sents)
         domain_interactions = [domain] * len(high_prob_sents)
+
+        print
+        print "domain: %s" % domain
+        print "High prob sents:"
+        print '\n'.join(high_prob_sents)
 
         # build up test vector
         vec.builder_clear()
