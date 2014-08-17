2228e7b6fdbd0e9a3d188e6a3851b0247e2d6d5d
byron wallace <byron.wallace@gmail.com>
2014-08-17 09:59:54 -0500

    adding n_jobs param back in

--- risk-of-bias/mt_mt.py
+++ risk-of-bias/mt_mt.py
@@ -74,7 +74,7 @@ def main(out_dir="results"):
     # setup sentence classifier
     tuned_parameters = {"alpha": np.logspace(-4, -1, 5), "class_weight": [{1: i, -1: 1} for i in np.logspace(0, 2, 5)]}
     # bcw: are we sure we want to do 'recall' here, and not (e.g.) F1?
-    sent_clf = GridSearchCV(SGDClassifier(loss="hinge", penalty="L2"), tuned_parameters, scoring='recall')
+    sent_clf = GridSearchCV(SGDClassifier(loss="hinge", penalty="L2"), tuned_parameters, scoring='recall', n_jobs=4)
 
     X_train = sent_vec.builder_fit_transform()
     y_train = sent_docs.y(uids_train)
@@ -100,7 +100,7 @@ def main(out_dir="results"):
 
 
     tuned_parameters = {"alpha": np.logspace(-4, -1, 5), "class_weight": [{1: i, -1: 1} for i in np.logspace(-1, 1, 5)]}
-    clf = GridSearchCV(SGDClassifier(loss="hinge", penalty="L2"), tuned_parameters, scoring='recall')
+    clf = GridSearchCV(SGDClassifier(loss="hinge", penalty="L2"), tuned_parameters, scoring='recall', n_jobs=4)
 
     # bcw: note that I've amended the y method to 
     # return interactions as well (i.e., domain strs)
